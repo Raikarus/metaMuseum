c6b527e64ca231892ab1b1bed0d7ee0e176afc0d
Raikarus <ivatim32@yandex.ru>
2022-12-18 15:42:43 +0300

    newBranch

--- ajax.php
+++
@@ -1,5 +0,0 @@
-<?php
-	echo "<img src=".$_POST['variable'].">";
-	unlink($_POST['variable']);
-	exit();
-?>
--- connect.php
+++
@@ -1,4 +0,0 @@
-<?php
-mysql_connect("localhost", "client", "sosi123"); // логин, пароль
-mysql_select_db("gachi_users"); // имя бызы
-?>
