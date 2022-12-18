9f6f422ec0163327c7e8b4c044d676ceeb672cbd
SpEcTrEeLeEe <daniilmanuylov239@gmail.com>
2022-12-18 18:46:20 +0300

    Fised bug with list

--- Spec_site/script.js
+++ Spec_site/script.js
@@ -1,32 +1,31 @@
+function flipflop( id )
+{
+
+   // id -- идентификатор элемента, который надо скрыть или показать
+
+   // получаем этот элемент
+   element = document.getElementById( id );
+
+   // если таковой в документе существует
+   if( element )   
+    // меняем ему видимость
+    element.style.display = element.style.display == "none" ? "" : "none";   
+}
+
 $(document).ready(function(){
-
-	function flipflop( id )
-	{
-
-		// id -- идентификатор элемента, который надо скрыть или показать
-
-		// получаем этот элемент
-		element = document.getElementById( id );
-
-		// если таковой в документе существует
-		if( element )   
-		 // меняем ему видимость
-		 element.style.display = element.style.display == "none" ? "" : "none";   
-	}
-   
-	let current_list = 1;
+	let current_page = 1;
 	update_grid();
 	$(".left").click(function()
 	{
-		if(current_list!=1)
+		if(current_page != 1)
 		{
-			current_list--;
+			current_page--;
 			update_grid();
 		}
 	});
 	$(".right").click(function()
 	{
-		current_list++;
+		current_page++;
 		update_grid();
 	});
 	
@@ -35,23 +34,23 @@ $(document).ready(function(){
 		let res = "";
 		for(var i = 1; i <= 6; i++)
 		{
-			if((current_list-1)*6+i < 10)
+			if((current_page-1)*6+i < 10)
 			{
-				res += '<li class = "photo_li" id = "img_00'+((current_list-1)*6+i)+'"><div class = "photo"></div><div class = "name">Название</div></li>';
+				res += '<li class = "photo_li" id = "img_00'+((current_page-1)*6+i)+'"><div class = "photo"></div><div class = "name">Название</div></li>';
 			}
 			else
 			{
-				if((current_list-1)*6+i < 100)
+				if((current_page-1)*6+i < 100)
 				{
-					res += '<li class = "photo_li" id = "img_0'+((current_list-1)*6+i)+'"><div class = "photo"></div><div class = "name">Название</div></li>'
+					res += '<li class = "photo_li" id = "img_0'+((current_page-1)*6+i)+'"><div class = "photo"></div><div class = "name">Название</div></li>'
 				}
 				else
 				{
-					res += '<li class = "photo_li" id = "img_'+((current_list-1)*6+i)+'"><div class = "photo"></div><div class = "name">Название</div></li>'	
+					res += '<li class = "photo_li" id = "img_'+((current_page-1)*6+i)+'"><div class = "photo"></div><div class = "name">Название</div></li>'	
 				}
 			}
 		}
 		$(".photos").html(res);
-		$("#current_page").html(current_list);
+		$("#current_page").html(current_page);
 	}
 });
