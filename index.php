327c58022360e55dd935391ccb555a95638e0b3b
Raikarus <ivatim32@yandex.ru>
2022-12-26 22:25:05 +0300

    Update index.php

--- index.php
+++ index.php
@@ -138,7 +138,7 @@
 							echo '<li class = "tag_group">                     
                         			<p class = "group_name">
                           				<input type="checkbox" name = "tags_on" class = tags_checkbox>
-                           				<a href = "javascript:flipflop(''.$row[0].'');">'.$row[0].'</a>
+                           				<a href = "javascript:flipflop('."'".$row[0]."'".');">'.$row[0].'</a>
                         			</p>
                         			<ul class = "tag_list" id = '.$row[0].' style="display: none;">';
 							$query = "SELECT name FROM gallery WHERE party='".$row[0]."'";
