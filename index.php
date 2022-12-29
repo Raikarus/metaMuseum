78121d3d631cd035c5427a15aa04f34babec8ef5
mikl14 <mikl.eli2002@gmail.com>
2022-12-29 21:25:57 +0300

    Update index.php

--- index.php
+++ index.php
@@ -92,9 +92,8 @@
                               echo '<li class = "tag_group">                     
                                           <p class = "group_name">
                                     
-                                                <a href = "javascript:flipflop('."'".$row[0]."'".');">'.$row[0].'+</a>
+                                                <a href = "javascript:flipflop('."'".$row[0]."'".');" style="font-family:Monotype Corsiva;color:green;font-size:25px">'.$row[0].'+</a>
                            
-
                                           </p>
                                           <ul class = "tag_list" id = '.$row[0].' style="display: none;">';
                               $query = "SELECT name FROM gallery WHERE party='".$row[0]."'";
