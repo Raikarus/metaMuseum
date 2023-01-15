4e9522ccb27c7a7c7e5050992d442c91229cbac9
Raikarus <ivatim32@yandex.ru>
2023-01-15 14:01:02 +0300

    Update ajax.php

--- ajax.php
+++ ajax.php
@@ -13,7 +13,7 @@
     function read() {
         $shl = 'exiftool 1.jpg';
         $res = shell_exec($shl);
-        echo "<pre>"+$res+"</pre>";
+        echo "<pre>$res</pre>";
         exit;
     }
 
