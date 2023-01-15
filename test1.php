442cc9897a185065a4f0753b9e41627be63364fd
mikl14 <mikl.eli2002@gmail.com>
2023-01-15 18:39:35 +0300

    Update test1.php

--- test1.php
+++ test1.php
@@ -4,6 +4,7 @@
          <?php
 
          $tag = "Artist";
+
          $var = "<rdf:Description rdf:about='20221225-2837_ss.jpg'
                   xmlns:et='http://ns.exiftool.ca/1.0/' et:toolkit='Image::ExifTool 12.16'
                   xmlns:IFD0='http://ns.exiftool.ca/EXIF/IFD0/1.0/'>
@@ -11,9 +12,8 @@
                 </rdf:Description>
                 </rdf:RDF>";
 
-          $pos = substr($var,strpos($var, $tag) + strlen($tag) + 1,strpos($var, 'Artist>'));
+          $pos = substr($var,strpos($var, $tag) + strlen($tag) + 1,strpos($var, $tag.'>'));
 
-          $sub = substr($pos,strpos($var, '>'),strpos($var, '1'));
                  echo $pos;
                  echo $sub;
           ?>
