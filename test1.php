e5f7693bb0c377d3eedd2ea89f97c1190ecc7c6d
mikl14 <mikl.eli2002@gmail.com>
2023-01-15 18:36:35 +0300

    Update test1.php

--- test1.php
+++ test1.php
@@ -2,13 +2,16 @@
         <body>
                  VAN DARCHOLM <BR>
          <?php
+
+         $tag = "Artist";
          $var = "<rdf:Description rdf:about='20221225-2837_ss.jpg'
                   xmlns:et='http://ns.exiftool.ca/1.0/' et:toolkit='Image::ExifTool 12.16'
                   xmlns:IFD0='http://ns.exiftool.ca/EXIF/IFD0/1.0/'>
                  <IFD0:Artist>GAY1</IFD0:Artist>
                 </rdf:Description>
                 </rdf:RDF>";
-          $pos = substr($var,strpos($var, 'Artist')+1,strpos($var, 'Artist>'));
+
+          $pos = substr($var,strpos($var, 'Artist') + strlen($tag),strpos($var, 'Artist>'));
 
           $sub = substr($pos,strpos($var, '>'),strpos($var, '1'));
                  echo $pos;
