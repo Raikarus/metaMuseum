c17fa2e282655ab381e5140fe98d68e4c0eb4f27
mikl14 <mikl.eli2002@gmail.com>
2023-01-15 18:44:24 +0300

    Update test1.php

--- test1.php
+++ test1.php
@@ -13,7 +13,7 @@
                 </rdf:RDF>";
 
           $pos = substr($var,strpos($var, $tag) + strlen($tag) + 1,strpos($var, $tag.'>'));
-
+          $sub = substr($var,strpos($var, $tag),strpos($var, $tag.'>') +strlen($tag)+1);
                  echo $pos;
                  echo $sub;
           ?>
