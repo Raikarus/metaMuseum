b982089d5208579f4dbcfd86b12d1e289330a983
mikl14 <mikl.eli2002@gmail.com>
2023-01-15 18:19:57 +0300

    Update test1.php

--- test1.php
+++ test1.php
@@ -2,8 +2,15 @@
         <body>
                  VAN DARCHOLM <BR>
          <?php
-         $var = 'Боб';
-                 echo $var;
+         $var = "<rdf:Description rdf:about='20221225-2837_ss.jpg'
+                  xmlns:et='http://ns.exiftool.ca/1.0/' et:toolkit='Image::ExifTool 12.16'
+                  xmlns:IFD0='http://ns.exiftool.ca/EXIF/IFD0/1.0/'>
+                 <IFD0:Artist>GAY1</IFD0:Artist>
+                </rdf:Description>
+                </rdf:RDF>";
+          $pos = strpos($var, 'Artist');
+
+                 echo $pos;
           ?>
           </body>
 </html>
