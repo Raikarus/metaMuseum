f821816b0f117bf5baf86a0aea8e18978083ea43
mikl14 <mikl.eli2002@gmail.com>
2023-01-16 20:35:57 +0300

    Update test1.php

--- test1.php
+++ test1.php
@@ -29,9 +29,9 @@
             $sub2 = substr($sub1,0,strpos($sub1, '</'));
 
            // echo "<pre>$sub2</pre>";
-            //$sub3 = substr($sub1,0,strpos($sub1, '>'));// для удаления
+            $sub3 = substr($sub1,0,strpos($sub1, '>'));// для удаления
 
-            $sub3 = substr($sub1,strpos($sub1, '<'),strpos($sub1, '>')).substr($sub1,0,strpos($sub1, '>'));
+            //$sub3 = substr($sub1,strpos($sub1, '<'),strpos($sub1, '>')).substr($sub1,0,strpos($sub1, '>'));
 
             //echo  htmlspecialchars("<pre>$text</pre>");
            
