f8e8b50e4ddc67741ec9d3d5871f728df3579710
Raikarus <ivatim32@yandex.ru>
2023-01-16 18:17:09 +0300

    Update index.php

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php
-$cn = pg_connect("host=localhost port=5432 dbname=postgres user=postgres password=schef2002");
+$cn = pg_connect("host=localhost port=1433 dbname=postgres user=postgres password=schef2002");
 $query = "SELECT * FROM pics";
 $res = pg_query($cn,$query);
 echo "<pre>$res</pre>";
