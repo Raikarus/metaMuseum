f9ddd0d212b8220bb09f0320f0cb83fd7ed7e7cd
mikl14 <mikl.eli2002@gmail.com>
2023-01-16 20:53:03 +0300

    Update test1.php

--- test1.php
+++ test1.php
@@ -7,36 +7,31 @@
                 ^^^^^^^^^^^^^^^^^^^^ <BR>
          <?php
 
-         $tag = "Artist";
+      //   $tag = "Artist";
          $tag2 = "softwareAgent";
-
-         $var = "<rdf:Description rdf:about='20221225-2837_ss.jpg'
-                  xmlns:et='http://ns.exiftool.ca/1.0/' et:toolkit='Image::ExifTool 12.16'
-                  xmlns:IFD0='http://ns.exiftool.ca/EXIF/IFD0/1.0/'>
-                 <IFD0:Artist>GAY1</IFD0:Artist>
-                </rdf:Description>
-                </rdf:RDF>";
 
           //$sub = substr($var,strpos($var, ':'.$tag),strpos($var,"</rdf:Description>"));
 
-          $pos = substr($var,strpos($var, $tag) + strlen($tag) + 1,strpos($var, $tag.'>'));
+         // $pos = substr($var,strpos($var, $tag) + strlen($tag) + 1,strpos($var, $tag.'>'));
+
           $filename ='test2.xmp';
            $text = file_get_contents($filename);
 
            // echo $text;
             $sub1 = substr($text,strpos($text, $tag2) + strlen($tag2) + 1,strpos($text, $tag2.'>'));
-                 
-            $sub2 = substr($sub1,0,strpos($sub1, '</'));
+///////////////////////////////////////////////////////////////////////////      
+
+            $sub2 = substr($sub1,0,strpos($sub1, '</')); //содержание тега
 
            // echo "<pre>$sub2</pre>";
 
-
+///////////////////////////////////////////////////////////////////////////////////
             $sub3 = substr($sub1,0,strpos($sub1, '>')+1);// для удаления
 
-            $sub4 = '<'.substr($sub3,strpos($sub3, '<')+2,strpos($sub3, '>')).$sub3;
+            $sub4 = '<'.substr($sub3,strpos($sub3, '<')+2,strpos($sub3, '>')).$sub3; // формируем всю строку для удаления
 
             //echo  htmlspecialchars("<pre>$text</pre>");
-            $delete_txt = substr($text,0,strpos($text,$sub4)).substr($text,strpos($text,$sub4)+strlen($sub4),strlen($text));
+            $delete_txt = substr($text,0,strpos($text,$sub4)).substr($text,strpos($text,$sub4)+strlen($sub4),strlen($text));//склеиваем основной текст обходя эту строку
 
              //echo htmlspecialchars($sub1);
 
@@ -46,29 +41,12 @@
 
 ///////////////////////////////////////////////////
                   /////////////////////////////////
-               $new_text = substr($text,0,strpos($text, $sub2))."Ваня гей".substr($text,strpos($text, $sub2)+strlen($sub2),strlen($text));
+               $new_text = substr($text,0,strpos($text, $sub2))."Ваня гей".substr($text,strpos($text, $sub2)+strlen($sub2),strlen($text)); //формируем строку где меняем все содержимое тега
               
                 //echo htmlspecialchars($new_text);
 
                 //вот этот new_text просто записываем в файл полной заменой 
 
-
-
-           /*    $fp = fopen('test3.xmp', 'w+');
-
-              
-                    // Записываем $somecontent в наш открытый файл.
-                    if (fwrite($fp, $new_text) === FALSE) {
-                        echo "Не могу произвести запись в файл ($filename)";
-                        exit;
-                    }
-                    else
-                    {
-                       echo "Ура! Записали ($new_text) в файл ($filename)";
-                    }
-                   
-
-                    fclose($fp);*/
 
            
           ?>
