713433e463f60698284b754929f2943719b114a3
Raikarus <ivatim32@yandex.ru>
2023-01-16 23:12:46 +0300

    Update index.php

--- index.php
+++ index.php
@@ -21,7 +21,7 @@ function upload_file($file, $nameFile='default', $upload_dir= 'img', $allowed_ty
   if(filesize($file['tmp_name']) > $max_filesize)
     return array('error' => 'файл слишком большой. максимальный размер '.intval($max_filesize/(1024*1024)).'мб');
 
-  if(!move_uploaded_file($file['tmp_name'],$filename))//$upload_dir.$prefix.$nameFile.$ext)) // Загружаем файл в указанную папку.
+  if(!move_uploaded_file($file['tmp_name'],$upload_dir.$filename))//$upload_dir.$prefix.$nameFile.$ext)) // Загружаем файл в указанную папку.
     return array('error' => 'При загрузке возникли ошибки. Попробуйте ещё раз.');
     $cn = pg_connect("host=localhost port=5432 dbname=postgres user=postgres password=schef2002");
   	$query = "INSERT INTO pics(title,fmt,fsize) VALUES('".$file['name']."','$ext',".$file['size'].")";
