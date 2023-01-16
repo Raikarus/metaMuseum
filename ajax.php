e4b7c2fec3c5de0e6869e6380556d27ab4a81bb8
Raikarus <ivatim32@yandex.ru>
2023-01-16 18:41:25 +0300

    Update ajax.php

--- ajax.php
+++ ajax.php
@@ -32,7 +32,7 @@
     function SQL()  {
         $cn = pg_connect("host=localhost port=5432 dbname=postgres user=postgres password=schef2002");
         $query = "SELECT * FROM tags";
-        $res = pg_query($cn,$res);
+        $res = pg_query($cn,$query);
         while($row=pg_fetch_object($res))
         {
             echo $row->tag_name."<br>";
