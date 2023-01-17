23441a9139390eff7ee6964c8963afda9bfa1746
mikl14 <mikl.eli2002@gmail.com>
2023-01-17 21:51:52 +0300

    Update test1.php

--- test1.php
+++ test1.php
@@ -57,7 +57,7 @@
                   if(strpos($text, $list[$i]))
                   {
                     $sub = substr($text, strpos($text, $list[$i]));
-                    $sub1 = substr($sub,0, strpos($text, ':'));
+                    $sub1 = substr($sub,0, strpos($text, ':'+10));
                     echo $sub1;
                     echo '<br>';
                   }
