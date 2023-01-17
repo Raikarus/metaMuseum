0d0e915db890dad2c2ee5a5684815d5b373eaba4
mikl14 <mikl.eli2002@gmail.com>
2023-01-17 21:34:28 +0300

    Update test1.php

--- test1.php
+++ test1.php
@@ -50,13 +50,12 @@
                  $list = array("DateTime","ModifyDate","FileModifyDate","ImageWidth","ImageHeight","Label","Title","AuthorPosition","ObjectName","By-lineTitle","UserComment","Description","ImageDescription","Headline","Caption-Abstract","Country","Country-PrimaryLocationName","State","Province-State","City","Subject","Keywords","Creator","Artist","Author","Identifier","Rights","Copyright","CopyrightNotice");
                 
                   $data_pos = 0;
-
-                 for($i = 0 ;$i <4;$i++) //count($list)
+                 for($i = 0 ;$i < count($list);$i++)
                  {
                   if(strpos($text, $list[$i]))
                   {
 
-                    echo substr($text, strpos($text, $list[$i]),strpos($text, $list[$i])+33);
+                    echo $list[$i];
                      echo '<br>';
                   }
                   
