e7f706c0e0e63647c29e30a00c087291332d443b
Raikarus <ivatim32@yandex.ru>
2023-01-17 18:56:11 +0300

    1

--- ajax.php
+++ ajax.php
@@ -1,4 +1,5 @@
 <?php
+    $cn = pg_connect("host=localhost port=5432 dbname=postgres user=postgres password=schef2002");
     if (isset($_POST['action'])) {
         switch ($_POST['action']) {
             case 'read':
@@ -45,7 +46,6 @@
     }
 
     function pics()  {
-        $cn = pg_connect("host=localhost port=5432 dbname=postgres user=postgres password=schef2002");
         $query = "SELECT * FROM pics";
         $res = pg_query($cn,$query);
         $row = pg_fetch_all($res);
@@ -55,7 +55,6 @@
     }
 
     function kwords() {
-        $cn = pg_connect("host=localhost port=5432 dbname=postgres user=postgres password=schef2002");
         $query = "SELECT * FROM kwords";
         $res = pg_query($cn,$query);
         $row = pg_fetch_all($res);
@@ -65,7 +64,6 @@
     }
 
     function pictags()  {
-        $cn = pg_connect("host=localhost port=5432 dbname=postgres user=postgres password=schef2002");
         $query = "SELECT * FROM pictags";
         $res = pg_query($cn,$query);
         $row = pg_fetch_all($res);
@@ -75,8 +73,6 @@
     }
 
     function remove()   {
-        $cn = pg_connect("host=localhost port=5432 dbname=postgres user=postgres password=schef2002");
-        
         $query = "DELETE FROM pics";
         pg_query($cn,$query);
         $query = "DELETE FROM pictags";
