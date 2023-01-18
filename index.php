6e6fc2f9c8d12e06cfa97df1c8f323cacbbe478b
Raikarus <ivatim32@yandex.ru>
2023-01-18 22:36:20 +0300

    Update index.php

--- index.php
+++ index.php
@@ -133,7 +133,7 @@ function AddToBd($filename,$fsize,$ext) {
       }
     }
   }
-  $md5 = md5_file("img/".$title);
+  $md5 = md5_file("img/".$filename);
   $query = "INSERT INTO pics(fmt,subscr,title,width,height,date,fsize,md5,rights) VALUES('".$ext."','".$subscr."','".$title."',$width,$height,'".$date."',$fsize,'".$md5."','".$rights."')";
   $res = pg_query($cn,$query);
   echo "ЗАПРОСИК $query<br>";
