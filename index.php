fa35c21e96c555e5f8fc69862dcd76f5e78f8c3f
Raikarus <ivatim32@yandex.ru>
2023-01-18 23:37:53 +0300

    Update index.php

--- index.php
+++ index.php
@@ -41,7 +41,7 @@ function Download() {
       }   
       $_FILES['imgfile'] = $files;
       echo "<pre>";
-      print_r($_FILES);
+      print_r($_FILES['imgfile']);
       echo "</pre>";
       /*
       foreach ($_FILES as $key => $value) {
