04a9361645583d0ea9317ac3b8939de23092fc67
Raikarus <ivatim32@yandex.ru>
2023-01-19 22:12:53 +0300

    Update index.php

--- index.php
+++ index.php
@@ -263,9 +263,8 @@ function LinkKeyword(){
 
     //ТУТ НАЗВАНИЕ НЕ КАК В БАЗЕ
 
-    $pic_id = substr($_POST['img_name'],0,strpos('.',$_POST['img_name']));
-    
-    
+    $pic_id = substr($_POST['img_name'],0,strpos($_POST['img_name'],'.'));
+
     foreach($_POST['kwords'] as $selected_kword)
     {
       $query="SELECT tag_id,tag_id_num FROM kwords WHERE kword_name='$selected_kword'";
