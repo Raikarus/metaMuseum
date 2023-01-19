76689beba9f28bb1254f55edf83b7e68cc11f5ec
mikl14 <mikl.eli2002@gmail.com>
2023-01-19 19:57:38 +0300

    Update ajax.php

--- ajax.php
+++ ajax.php
@@ -16,12 +16,7 @@ function check()
     //print_r($str);
     for($i = 0; $i < count($str)-1;$i++)
     {
-        echo '<li class = "compilation_li">
-                           <button class = "comp_li_button" data-val= "0" name ="img">
-                               <div class = "comp_li_photo" style="background-image:url('."'".'img/'.$str[$i]."'".'"></div>
-                               <div class = "comp_li_name">'.$str[$i].'</div>   
-                           </button>        
-                         </li>';' ;
+        echo '<div class = "comp_li_photo" name ="img" style="background-image:url('."'".'img/'.$str[$i]."'".'" data-val = "'.$img.'""></div>' ;
     }
 	
 	
