40fab15f5d24c21b6da20a1f177a3c1879e4b0b4
Raikarus <ivatim32@yandex.ru>
2023-01-19 16:41:59 +0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -128,14 +128,16 @@ $(document).ready(function(){
            var current_page = Number($('#current_page').data('val'));
            if(clickBtnValue == "right") current_page+=1;
            else if (current_page!=1) current_page-=1;
-           $('#current_page').data('val',current_page).html(current_page).attr('data-val',current_page);
            var ajaxurl = 'ajax.php';
            data =  {'action': 'update_grid', 'current_page': current_page,'size':size};
            $.post(ajaxurl, data, function (response) {
-              if(response != 'error') $('#wrapping').html(response);
+              if(response != 'error') {
+                alert(response);
+                $('#wrapping').html(response);
+                $('#current_page').data('val',current_page).html(current_page).attr('data-val',current_page);
+              }
               else {
-                $('#current_page').data('val',current_page-1).html(current_page-1).attr('data-val',current_page-1);
-                preload();
+                current_page-=1;
               } 
            });
     });
