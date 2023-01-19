9bf01cd0b2102f1b571768be433bebf192f5e0c7
Raikarus <ivatim32@yandex.ru>
2023-01-19 20:29:27 +0300

    Update ajax.php

--- ajax.php
+++ ajax.php
@@ -135,6 +135,9 @@
                 $res2 = pg_query($cn,$query2);
                 $tag_id_num_array_from_pic_id = pg_fetch_all($res2);
                 $ok = "ok";
+                echo "<pre>";
+                print_r($tag_id_num_array_from_pic_id);
+                echo "</pre>";
                 for ($i=0; $i < count($tag_id_num_array); $i++) { 
                     if($result_tags_invers_arr[$i] == "0")
                     {
