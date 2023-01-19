ded16b9fb61a00fdaddab389ae5cb49206b0dd8c
mikl14 <mikl.eli2002@gmail.com>
2023-01-19 17:16:54 +0300

    Update image_tags.php

--- image_tags.php
+++ image_tags.php
@@ -90,7 +90,7 @@ function moveZeros(arr) {
                ss += arr[i] + '|';
             }
             
-
+            ss = arr[count];
             var ajaxurl = 'ajax.php';
             data =  {'action': 'set_img','img_string':ss};
             $.post(ajaxurl, data, function (response) {
