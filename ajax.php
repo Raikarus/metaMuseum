8c8623253587e6b8408f2ab1b9b1f1d97cfe8859
Raikarus <ivatim32@yandex.ru>
2023-01-20 02:07:57 +0300

    Update ajax.php

--- ajax.php
+++ ajax.php
@@ -154,11 +154,11 @@
                     }
                 }
 
-                 echo "$pic_id <pre>";
-                 print_r($result_tags_invers_arr);
-                 print_r($tag_id_num_array);
-                 print_r($tag_id_num_array_from_pic_id);
-                 echo "</pre>";
+                 // echo "$pic_id <pre>";
+                 // print_r($result_tags_invers_arr);
+                 // print_r($tag_id_num_array);
+                 // print_r($tag_id_num_array_from_pic_id);
+                 // echo "</pre>";
 
                 if($ok == "ok") 
                 {
