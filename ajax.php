d25e7948d9537c4e0703d75cabc207a0b9e88de0
mikl14 <mikl.eli2002@gmail.com>
2023-01-19 18:59:27 +0300

    Update ajax.php

--- ajax.php
+++ ajax.php
@@ -13,8 +13,11 @@ $status = 0;
 function check()
 {
     $str = explode('|', $_POST['img_string']);
-
-	echo '<div class = "comp_li_photo" name ="img" style="background-image:url('."'".'img/'.$str[0]."'".'" data-val = "'.$img.'""></div>' ;
+    for($i = 0; $i < count(str);$i++)
+    {
+        echo '<div class = "comp_li_photo" name ="img" style="background-image:url('."'".'img/'.$str[$i]."'".'" data-val = "'.$img.'""></div>' ;
+    }
+	
 	
 }
 ?>
