00f3ba43c59e2f36e232234fe0b19557bf4f0433
SpEcTrEeLeEe <daniilmanuylov239@gmail.com>
2023-01-20 19:26:49 +0300

    /

--- style.php
+++ style.php
@@ -10,7 +10,7 @@
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.0/jquery.min.js"></script>
 <script type="text/javascript" src = "js/script.js"></script>
 <script>
-autocomplete(document.getElementById("myInput"), countries);
+   autocomplete(document.getElementById("myInput"), countries);
 </script>
 <title>Главная</title>
 <style>
