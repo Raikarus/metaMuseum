1a61989a79f35d81ec46fdffd53f1de0b72c3460
Raikarus <ivatim32@yandex.ru>
2023-01-20 06:02:30 +0300

    Update ajax.php

--- ajax.php
+++ ajax.php
@@ -215,42 +215,48 @@
         }
         else
         {
+            $start = 0;
+            $end = 6;
+            switch ($_POST['size']) {
+                case '3x2':
+                    $start = ($_POST['current_page']-1)*6;
+                    $end = $start + 6;
+                    break;
+                case '4x3':
+                    $start = ($_POST['current_page']-1)*12;
+                    $end = $start + 12;
+                    break;
+                case '5x4':
+                    $start = ($_POST['current_page']-1)*20;
+                    $end = $start + 20;
+                    break;
+            }
             $cn = pg_connect("host=localhost port=5432 dbname=postgres user=postgres password=schef2002");
             $podborka = explode("|", $_POST['podborka']);
             if($_POST['podborka'])
             {
                 $query = "SELECT pic_id,fmt,title FROM pics WHERE pic_id=$podborka[0]";
-                for ($i=1; $i < count($podborka)-1; $i++) { 
-                    $query .= "UNION ALL SELECT pic_id,fmt,title FROM pics WHERE pic_id=$podborka[$i]";
-                }
-                $start = 0;
-                $end = 6;
-                switch ($_POST['size']) {
-                    case '3x2':
-                        $start = ($_POST['current_page']-1)*6;
-                        $end = $start + 6;
-                        break;
-                    case '4x3':
-                        $start = ($_POST['current_page']-1)*12;
-                        $end = $start + 12;
-                        break;
-                    case '5x4':
-                        $start = ($_POST['current_page']-1)*20;
-                        $end = $start + 20;
-                        break;
-                }
                 $res = pg_query($cn,$query);
-                for ($i=$start; $i < $end; $i++) { 
-                 $pic_id = pg_fetch_result($res, $i, 0);
-                 $fmt = pg_fetch_result($res, $i, 1);
-                 $title = pg_fetch_result($res, $i, 2);
-                 if($pic_id) echo "<li class='photo_li' data-id=$pic_id><div class='photo' style='background-image:url(".'"img/'.$pic_id.".".$fmt.'"'.")'></div><div class='name'>$title</div></li>";  
+                if(pg_fetch_result($res, $start, 0)){
+                    $query = "SELECT pic_id,fmt,title FROM pics WHERE pic_id=$podborka[0]";
+                    for ($i=1; $i < count($podborka)-1; $i++) { 
+                        $query .= "UNION ALL SELECT pic_id,fmt,title FROM pics WHERE pic_id=$podborka[$i]";
+                    }
+                    
+                    $res = pg_query($cn,$query);
+                    for ($i=$start; $i < $end; $i++) { 
+                     $pic_id = pg_fetch_result($res, $i, 0);
+                     $fmt = pg_fetch_result($res, $i, 1);
+                     $title = pg_fetch_result($res, $i, 2);
+                     if($pic_id) echo "<li class='photo_li' data-id=$pic_id><div class='photo' style='background-image:url(".'"img/'.$pic_id.".".$fmt.'"'.")'></div><div class='name'>$title</div></li>";  
+                    }
+                }
+                else
+                {
+                    echo "error";
                 }
             }
-            else
-            {
-                echo "error";
-            }
+
         }
     }
 
