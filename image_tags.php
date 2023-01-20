a323624eb5346b3dfc3759ceea82cd3a02a8f96c
mikl14 <mikl.eli2002@gmail.com>
2023-01-20 19:30:29 +0300

    Update image_tags.php

--- image_tags.php
+++ image_tags.php
@@ -120,7 +120,7 @@ function moveZeros(arr) {
         });
 
          $('.switch').click(function(){
-            $(document.getElementsByName("img")[0]).css('display:none !important');
+            $(document.getElementsByName("img")[0]).css('display','none');
               console.log(document.getElementsByName("img")[0]);
               status++;
               if(status == 1) //download mode
