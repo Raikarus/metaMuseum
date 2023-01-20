63b8afe4aa661466c0eedf131d02eb794547ad32
Raikarus <ivatim32@yandex.ru>
2023-01-20 07:13:31 +0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -329,7 +329,7 @@ $(document).ready(function(){
   });
 
   $("#save_podborka").click(function(){
-    
+    alert("1");
     $("#name_for_podborka_fon").fadeTo(1000);
     // var ajaxurl = 'ajax.php';
     // data =  {'action': 'save_podborka'}; 
