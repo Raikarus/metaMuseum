5b60e9ccc756ba64807bcafdf2a22cbad051851d
SpEcTrEeLeEe <daniilmanuylov239@gmail.com>
2023-01-20 22:07:56 +0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -308,7 +308,7 @@ $(document).ready(function(){
       wrap.style.gridTemplateColumns = "repeat(5, 1fr)";
       wrap.style.gridTemplateRows = "repeat(4, 1fr)";
       limit_of_pages = 20;
-      $(".name").css("padding-bottom", "2px" ).css("font-size" , "100px");
+      $(".name").css("padding-bottom", "2px" ).css("font-size" , "5px");
       size = '5x4';
       load();
    });
