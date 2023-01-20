30cde16006670547d21905026a3eacfc351fa695
Raikarus <ivatim32@yandex.ru>
2023-01-21 00:51:53 +0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -243,6 +243,7 @@ $(document).ready(function(){
   $('#addtag').on("click",function()
   {
     elem = $('.kword_solo[data-dag="'+$('#myInput').val()+'"]');
+    alert('.kword_solo[data-dag="'+$('#myInput').val()+'"]');
     var index = result_tags.indexOf($(elem).data("tag"));
     if (index >= 0) {
       result_tags.splice(index, 1);
