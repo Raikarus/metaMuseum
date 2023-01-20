3bcbc13818b8d6b3397c2cf5431b3d943edd20d3
mikl14 <mikl.eli2002@gmail.com>
2023-01-20 18:33:17 +0300

    Update downloader.php

--- downloader.php
+++ downloader.php
@@ -32,83 +32,6 @@
 <link rel="stylesheet" href="css/style.css"  type="text/css">
 <script type="text/javascript" src = "js/jq.js"></script>
 <script type="text/javascript" src = "js/script.js"></script>
- <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.0/jquery.min.js"></script>
-
-    <script>
-      var count = 0;
-      let arr = [(document.getElementsByName("img")).length];
-function moveZeros(arr) {
-    let upperBound = arr.length;
-    for (let i = 0; i < upperBound; i++) {
-        if (arr[i] === 0) {
-            arr.push(0);
-            arr.splice(i, 1);
-            upperBound--;
-            i--;
-        }
-    }
-    return arr;
-}
- 
-    $(document).ready(function(){
-        $('.comp_li_button').click(function(){
-            var clickBtnValue = $(this).data('val');
-            
-           
-            if(clickBtnValue=='0')
-            {
-             // $(this).css('outline','5px solid #24B47E');
-               $(this).css('background-color', '#24B47E');
-              $(this).data('val','1');
-          
-               arr[count] =  String($(this).data('img'));
-              
-              if(count < document.getElementsByName("img").length)
-              {
-               count++;
-              }
-
-            }
-            else
-            {
-              //$(this).css('outline','none');
-               $(this).css('background-color', 'rgba(255, 255, 255, 0)');
-              $(this).data('val','0');
-
-              for(var i =0;i <count;i++ )
-              {
-               if(arr[i] == $(this).data('img'))
-               {
-                   arr[i] = 0;
-                   moveZeros(arr);
-                   break;
-               }
-              }
-             
-              count--;
-            }
-            
-          
-            var ss = "";
-
-            for(var i = 0; i < count;i++)
-            {
-               ss += arr[i] + '|';
-                 
-            }
-             console.log(ss);
-           
-             
-            var ajaxurl = 'ajax.php';
-            data =  {'action': 'set_img','img_string':ss};
-            
-            $.post(ajaxurl, data).done(function (response) {
-                  $('#wrapping').html(response);
-            });
-        });
-    });
-
-    </script>
 <title>Главная</title>
 </head>
 <body>
@@ -128,19 +51,28 @@ function moveZeros(arr) {
    </header>
 
 <div class = "main">
-      <div class = "main_center">
+      <div class = "main_left">
          <ul class = "photos" id = "wrapping">
-         	
-		   </ul>
-      <div class = "left_right_but">
-         <button class = "left">
-            ←
-         </button>
-         <div id = "current_page">1</div>
-         <button class="right">
-            →
-         </button>    
-      </div>
+            <?php
+             $dir='./img';
+            $files = scandir($dir);
+            foreach($files as $n => $img){
+               if ($img != '.' && $img != '..') echo '<li class = "photo_li" >
+                           <div class = "photo" style="background-image:url('."'".'img/'.$img."'".'"></div>
+                           <div class = "name">'.$img.'</div>           
+                      </li>';
+            }
+            ?>
+      </ul>
+         <div class = "left_right_but">
+            <button class = "left">
+               ←
+            </button>
+            <div id = "current_page">1</div>
+            <button class="right">
+               →
+            </button>    
+         </div>
       </div>
       <div class = "main_right">
          <div class = "all_tags">
@@ -150,77 +82,66 @@ function moveZeros(arr) {
                      <input type = "submit" name = "submit" class = "submit" value="ADD">
                   </form>
                </div>
-               <div class = "tags" >
-               <?php 
-                          
-                          $query = "SELECT DISTINCT party FROM gallery";
-                          $res = pg_query($cn,$query);
-                       
-                          while($row=pg_fetch_row($res))
-                          {
-                              $x = rand(5, 15);
-                             echo '<li class = "tag_group">                     
-                                         <p class = "group_name">
-                                   
-                                               <a href = "javascript:flipflop('."'".$row[0]."'".');" style="font-size:'.$x.'px">'.$row[0].'+</a>
-                          
-                                         </p>
-                                         <ul class = "tag_list" id = '.$row[0].' style="display: none;">';
-                             $query = "SELECT name FROM gallery WHERE party='".$row[0]."'";
-                             $res2 = pg_query($cn,$query);
-                             while($row2=pg_fetch_row($res2))
-                             {
-                                echo '<li class = "list_item"><a href = "#" data-en = 0 data-tag = '.$row2[0].'>'.$row2[0].'</a></li>';
-                             }
-                             echo "</ul></li>";
-                          }
-                       ?>
-                       </ul>
+               <div class = "tags">
 
-                          ?>
-                          <!--	<ul class = "list_of_groups">
-                             <?php
-                          $query = "SELECT DISTINCT party FROM gallery";
-                          $res = pg_query($cn,$query);
-                          while($row=pg_fetch_row($res))
-                          {
-                             echo '<li class = "tag_group">                     
-                                         <p class = "group_name">
-                                     
-                                               <a href = "javascript:flipflop('."'".$row[0]."'".');">'.$row[0].'+</a>
+                           <?php 
                           
-
-                                         </p>
-                                         <ul class = "tag_list" id = '.$row[0].' style="display: none;">';
-                             $query = "SELECT name FROM gallery WHERE party='".$row[0]."'";
-                             $res2 = pg_query($cn,$query);
-                             while($row2=pg_fetch_row($res2))
-                             {
-                                echo '<li class = "list_item"><a href = "#" data-en = 0 data-tag = '.$row2[0].'>'.$row2[0].'</a></li>';
-                             }
-                             echo "</ul></li>";
-                          }
-                       ?>
-                       </ul> --> 
+                           $query = "SELECT DISTINCT party FROM gallery";
+                           $res = pg_query($cn,$query);
                         
-               </div>
-               <div class = "choosen_tags">
-                  <ul class = "wrap">
-                  </ul>
-                  <?php 
-                          $tags  = "japanese hentai lesbian milf korean asian step_mom massage anal ebony big_ass teen threesome public anime creampie ";
-                          $tag = explode(" ", $tags);
-                          
-                        for($i = 0; $i < 16;$i++)
-                        {
-                           $size = rand(10, 40);
-                            echo '<span class = "cloud_tag" style="font-size:'.$size.'px;">'.$tag[$i]."($size)".'</span>';
-                            echo "\n";
-                        }
-                        ?> 
+                           while($row=pg_fetch_row($res))
+                           {
+                               $x = rand(5, 15);
+                              echo '<li class = "tag_group">                     
+                                          <p class = "group_name">
+                                    
+                                                <a href = "javascript:flipflop('."'".$row[0]."'".');" style="font-size:'.$x.'px">'.$row[0].'+</a>
+                           
+                                          </p>
+                                          <ul class = "tag_list" id = '.$row[0].' style="display: none;">';
+                              $query = "SELECT name FROM gallery WHERE party='".$row[0]."'";
+                              $res2 = pg_query($cn,$query);
+                              while($row2=pg_fetch_row($res2))
+                              {
+                                 echo '<li class = "list_item"><a href = "#" data-en = 0 data-tag = '.$row2[0].'>'.$row2[0].'</a></li>';
+                              }
+                              echo "</ul></li>";
+                           }
+                        ?>
+                        </ul>
 
+                           ?>
+                           <!--  <ul class = "list_of_groups">
+                              <?php
+                           $query = "SELECT DISTINCT party FROM gallery";
+                           $res = pg_query($cn,$query);
+                           while($row=pg_fetch_row($res))
+                           {
+                              echo '<li class = "tag_group">                     
+                                          <p class = "group_name">
+                                    
+                                                <a href = "javascript:flipflop('."'".$row[0]."'".');">'.$row[0].'+</a>
+                           
+
+                                          </p>
+                                          <ul class = "tag_list" id = '.$row[0].' style="display: none;">';
+                              $query = "SELECT name FROM gallery WHERE party='".$row[0]."'";
+                              $res2 = pg_query($cn,$query);
+                              while($row2=pg_fetch_row($res2))
+                              {
+                                 echo '<li class = "list_item"><a href = "#" data-en = 0 data-tag = '.$row2[0].'>'.$row2[0].'</a></li>';
+                              }
+                              echo "</ul></li>";
+                           }
+                        ?>
+                        </ul> --> 
                </div>
-         </div>      
+         </div>
+         <div class = "choosen_tags">
+            <ul class = "wrap">
+               
+            </ul>
+         </div>
       </div>
    </div>
 </body>
