edcbae002201e95c17373bcc34e3374ef5e2fba7
mikl14 <mikl.eli2002@gmail.com>
2023-01-20 20:02:21 +0300

    Update ajax.php

--- ajax.php
+++ ajax.php
@@ -23,7 +23,7 @@ function check()
 	}
     else
     {
-         echo '<img src="document.png" alt="Girl in a jacket" width="500" height="600">' ;
+         echo '<img src="document.png" alt="s" width="100%" height="100%">' ;
     }
 	
 }
