65918698b7dcb0aaed5e5e129d1626065b2a78ce
Raikarus <ivatim32@yandex.ru>
2023-01-20 04:53:41 +0300

    Update ajax.php

--- ajax.php
+++ ajax.php
@@ -197,8 +197,11 @@
 	         $pic_id = pg_fetch_result($res, $i, 0);
 	         $fmt = pg_fetch_result($res, $i, 1);
 	         $title = pg_fetch_result($res, $i, 2);
-             if(!in_array($pic_id, $podborka))echo "<li class='photo_li' data-id=$pic_id><div class='photo' style='background-image:url(".'"img/'.$pic_id.".".$fmt.'"'.")'></div><div class='name'>$title</div></li>";   
-             else echo "<li class='photo_li' style='outline:3px solid red;outline-offset:-3px' data-id=$pic_id><div class='photo' style='background-image:url(".'"img/'.$pic_id.".".$fmt.'"'.")'></div><div class='name'>$title</div></li>";   
+             if($pic_id)
+             {
+                 if(!in_array($pic_id, $podborka))echo "<li class='photo_li' data-id=$pic_id><div class='photo' style='background-image:url(".'"img/'.$pic_id.".".$fmt.'"'.")'></div><div class='name'>$title</div></li>";   
+                 else echo "<li class='photo_li' style='outline:3px solid red;outline-offset:-3px' data-id=$pic_id><div class='photo' style='background-image:url(".'"img/'.$pic_id.".".$fmt.'"'.")'></div><div class='name'>$title</div></li>";   
+             }
 	        }
     	}
     	else
