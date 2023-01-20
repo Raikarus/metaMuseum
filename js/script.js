544be86fda12835285c8da037d894c3a3d8204e6
Raikarus <ivatim32@yandex.ru>
2023-01-20 09:09:44 +0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -117,7 +117,7 @@ $(document).ready(function(){
       }
     });
 
-  $('.tag_group .group_name ').on("click",".kword_solo",function (){
+  $('.tag_group .group_name').on("click",".kword_solo",function (){
     var index = result_tags.indexOf($(this).data("tag"));
     if (index >= 0) {
       result_tags.splice(index, 1);
