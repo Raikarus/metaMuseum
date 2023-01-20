e00967e896a32514e01a945290bf6f68c112afe5
SpEcTrEeLeEe <daniilmanuylov239@gmail.com>
2023-01-21 02:07:56 +0300

    Update tags.php

--- tags.php
+++ tags.php
@@ -65,6 +65,7 @@
 <body>
 <a href="index.php">Дом</a>
 <a href="tags.php">Тэги</a>
+<a href="style.php">Главная страница</a>
 <br>
 <input type="text" id="img_name" placeholder="Название изображения">
 <input type="submit" class="button" name="read" value="read" />
