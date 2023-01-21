020846c352ed919bdf5a0f3ea0bf2bf7ab7d34c7
mikl14 <mikl.eli2002@gmail.com>
2023-01-22 02:16:44 +0300

    ф

---
+++ test1.php
@@ -0,0 +1,15 @@
+<?php
+
+?ead>
+<meta charset="utf-8">
+<meta http-equiv="Content-Type" content="text/html">
+<link rel="stylesheet" href="css/style.css"  type="text/css">
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.0/jquery.min.js"></script>
+<script type="text/javascript" src = "js/script.js"></script>
+<title>Главная</title>
+
+</head>
+<body>
+    <a href="https://www.microsoft.com/en-us/windows/microsoft-edge">Microsoft Edge</a>.</p>
+</body>
+</html>
