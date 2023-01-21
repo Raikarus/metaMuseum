232c058e669173db11ac8369a8e1dc0c0162a8f6
mikl14 <mikl.eli2002@gmail.com>
2023-01-21 15:49:42 +0300

    Update test1.php

--- test1.php
+++ test1.php
@@ -1,114 +1,109 @@
-  <head>
-    <meta charset="utf-8">
-    <title>ХАХАХАХАХАХА</title>
+<?php
 
+?>
+<!DOCTYPE HTML>
+<html>
+<head>
+<meta charset="utf-8">
+<meta http-equiv="Content-Type" content="text/html">
+<link rel="stylesheet" href="css/style.css"  type="text/css">
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.0/jquery.min.js"></script>
+<script type="text/javascript" src = "js/script.js"></script>
+<title>Главная</title>
 
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.0/jquery.min.js"></script>
+</head>
+<body>
+<!-- ТУТ ПОМЕНЯТЬ СТИЛИ -->
+         <div id = "name_podborka_form_back">
+            <div id = "name_podborka_form">
+               <h3>Введите название:</h3>
+               <input id = "podborka_name" type="text" name="name_for_podborka">
+               <input id = "create_podborka" type="submit" value = "Создать">
+               <div id = "response"></div>
+            </div>
+         </div>
+<!-- ТУТ ПОМЕНЯТЬ СТИЛИ -->
+<header>
+      <div class = "container">
+         <div class = "logo_container">
+            <a class = "logo" href = "/?form=home">
+               DATABASE
+            </a>
+         </div>
+         <ul class = username_exit>
+            <li class = "usit_elem username">Username</li>
+            <li class = "usit_elem exit"><a class = "exit" href = "#">Exit</a></li>
+         </ul>
+      </div>
+   </header>
 
-    <script>
+<div class = "main">
+      <div class = "main_left">
 
-    $(document).ready(function(){
-        $('.button').click(function(){
-            var clickBtnValue = $(this).val();
-            var ajaxurl = 'ajax.php';
-            data =  {'action': clickBtnValue};
-            $.post(ajaxurl, data, function (response) {
-                  $('#out').html(response);
-            });
-        });
-    });
+         <ul class = "photos" id = "wrapping">
+                   </ul>
+         <div class = "left_right_but">
+            <button class = "button left" data-val="left">
+               ←
+            </button>
+            <div id = "current_page" data-val="1">1</div>
+            <button class = "button right" data-val="right">
 
-    </script>
-  </head>
+               →
+            </button>
+         </div>
+      </div>
+      <div class = "main_right">
+         <div class = "all_tags">
+               <form autocomplete = "off" class = "searching">
+                  <div class = "autocomplete">
+                     <input id = "myInput" class = "searchbar" type = "text" placeholder = "Search tags!">
 
+                  </div>
+                  <input type = "button" id = "addtag" class = "addtag" value = "ADD">
 
-  <html>
-        <body>
-                ^^^^^^^^^^^^^^^^^^^^ <BR>
-         <?php
+               </form>
+               <div class = "tags">
+                <ul class = "list_of_groups">
+                     <!-- Два режима: kwords И подборки (SELECTIONS) -->
+                                           </ul>
+               </div>
+         </div>
+         <div class = "choosen_tags">
+            <ul class = "wrap">
 
-         $str = "Памятники";
+            </ul>
+         </div>
+      </div>
+   </div>
 
-         $kword_names = explode(",", $str);
-          echo "<b style='color:green'><pre>";
-          print_r($kword_names);
-          echo "</pre></b><br>";
-
-        
-/*
-           // echo $text;
-            $sub1 = substr($text,strpos($text, $tag2) + strlen($tag2) + 1,strpos($text, $tag2.'>'));
-///////////////////////////////////////////////////////////////////////////      
-
-            $sub2 = substr($sub1,0,strpos($sub1, '</')); //содержание тега
-
-            //echo "<pre>$sub2</pre>";
-
-///////////////////////////////////////////////////////////////////////////////////
-            $sub3 = substr($sub1,0,strpos($sub1, '>')+1);// для удаления
-
-            $sub4 = '<'.substr($sub3,strpos($sub3, '<')+2,strpos($sub3, '>')).$sub3; // формируем всю строку для удаления
-
-            //echo  htmlspecialchars("<pre>$text</pre>");
-            $delete_txt = substr($text,0,strpos($text,$sub4)).substr($text,strpos($text,$sub4)+strlen($sub4),strlen($text));//склеиваем основной текст обходя эту строку
-
-             //echo htmlspecialchars($sub1);
-
-            // echo htmlspecialchars($delete_txt);
-              //   echo $pos;
-                
-
-///////////////////////////////////////////////////
-                  /////////////////////////////////
-               $new_text = substr($text,0,strpos($text, $sub1))."Ваня гей".substr($text,strpos($text, $sub1)+strlen($sub2),strlen($text)); //формируем строку где меняем все содержимое тега
-              
-                echo htmlspecialchars($new_text);
-
-                //вот этот new_text просто записываем в файл полной заменой 
-
-////////////////////////////////////////////////////////
-*/
-               /*  $filename1 ='test2.xmp';
-                 $text1 = file_get_contents($filename);
-*/
-          /*       $list = array("DateTime","ModifyDate","FileModifyDate","ImageWidth","ImageHeight","Label","T4444itle","AuthorPosition","ObjectName","By-lineTitle","UserComment","Description","ImageDescription","Headline","Caption-Abstract","Country","Country-PrimaryLocationName","State","Province-State","City","Subject","Keywords","Creator","Artist","Author","Identifier","Rights","Copyright","CopyrightNotice");
-                
-                  $data_pos = 0;
-                  $sub = " " ;
-                  $sub1 = " " ;
-                   $sub2 = " " ;
-                 for($i = 0 ;$i < count($list);$i++)
-                 {
-                  if(strpos($text, $list[$i]))
-                  {
-                    $sub = substr($text, strpos($text, $list[$i]));
-                    $sub1 = substr($sub,0, strpos($text, ' : '));
-                    $sub2 = substr($sub,strpos($text, ':'), strpos($text,"\n"));
-
-                    echo $sub1." : ".$sub2;
-                    echo '<br>';
-                  }
-                  
-                 }
-                  echo htmlspecialchars("<pre>$text</pre>");
-           */
-
-
-                /*  $str =  "2023:01:18 18:38:55+00:00";
-
-                  $str1 = substr($str,0, strpos($str, ' '));
-                  $str1 = str_replace(':','-', $str1);
-
-                  $str1 =  $str1.substr($str,strpos($str, ' '));
-                  $str1 = substr($str1,0,strpos($str1,'+'));
-                  echo $str1;*/
-          //        $str = 0;
-         //        echo '<button id="#example1" value= "read" class="button">Войти</button>';
-              
-
-
-                 
-          ?>
-    
-          </body>
+   <footer>
+      <div class = "switch_mod">
+         <input type = "submit" name = "mods" id = "mod_gallery"value="Галерея">
+         <input type = "submit" name = "mods" id = "mod_finder" value="Подборка">
+      </div>
+      <div class = "instruments" id = "gal_ins">
+         <ul class = "panel">
+            <li><input id = "add_to_podborka" type = "submit" name = "funtional" value="Добавить в подборку"></li>
+            <li><input class = "select_all" type = "submit" name = "funtional" value="Выделить всё"></li>
+            <li><input class = "unselect_all" type = "submit" name = "funtional" value="Снять выделение"></li>
+            <!-- <li><input type = "submit" name = "funtional" value="Отменить"></li> -->
+         </ul>
+      </div>
+      <div class = "instruments" id = "fin_ins">
+         <ul class = "panel">
+            <li><input type = "submit" name = "funtional" value="Скачать"></li>
+            <li><input id = "save_podborka" type = "submit" name = "funtional" value="Сохранить в подборку"></li>
+            <li><input id="delete_from_podborka" type = "submit" name = "funtional" value="Удалить из подборки"></li>
+            <li><input class = "select_all" type = "submit" name = "funtional" value="Выделить всё"></li>
+            <li><input class = "unselect_all" type = "submit" name = "funtional" value="Снять выделение"></li>
+         </ul>
+      </div>
+      <div class = "switch_size">
+         <input type = "submit" name = "sizes" id = "size_small" value = "5x4">
+         <input type = "submit" name = "sizes" id = "size_normal" value = "4x3">
+         <input type = "submit" name = "sizes" id = "size_big" value = "3x2">
+      </div>
+   </footer>
+</body>
 </html>
