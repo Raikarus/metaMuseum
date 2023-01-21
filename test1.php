60e5cdddb465084af8b3e495afd4d8eb7f5a26bd
mikl14 <mikl.eli2002@gmail.com>
2023-01-21 16:38:25 +0300

    a

--- test1.php
+++ test1.php
@@ -6,7 +6,7 @@
 <head>
 <meta charset="utf-8">
 <meta http-equiv="Content-Type" content="text/html">
-<link rel="stylesheet" href="css/style.css"  type="text/css">
+<link rel="stylesheet" href="css/style2.css"  type="text/css">
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.0/jquery.min.js"></script>
 <script type="text/javascript" src = "js/script.js"></script>
 <title>Главная</title>
