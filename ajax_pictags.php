00b91a9a43d9874d02c4eb635b9186c0dda775d8
Raikarus <ivatim32@yandex.ru>
2023-01-23 00:12:15 +0300

    Update ajax_pictags.php

--- ajax_pictags.php
+++ ajax_pictags.php
@@ -117,6 +117,7 @@ function load_cross_kwords()
             }
             $query .= ") GROUP BY tag_id_num HAVING COUNT(tag_id_num)>1";
             $res = pg_query($cn,$query);
+            echo "<script>console.log($query)</script>"
             if($row = pg_fetch_object($res))
             {
                 $tag_id_num = $row->tag_id_num;
@@ -126,6 +127,7 @@ function load_cross_kwords()
                     $tag_id_num = $row->tag_id_num;
                     $query .= " OR tag_id_num=$tag_id_num";
                 }
+                echo "<script>console.log($query)</script>";
                 $res = pg_query($cn,$query);
                 
                 while($row = pg_fetch_object($res))
