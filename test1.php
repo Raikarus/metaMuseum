77839bf7ec10517ad5250d7448feabb9b7647bb0
mikl14 <mikl.eli2002@gmail.com>
2023-01-22 18:34:06 +0300

    Update test1.php

--- test1.php
+++ test1.php
@@ -191,16 +191,7 @@ function moveZeros(arr) {
                   </form>
                </div>
                <div class = "tags" >
-                <form action="select1.php" method="post">
-                 <p><select size="3" multiple name="hero[]">
-                  <option disabled>Выберите героя</option>
-                  <option value="Чебурашка">Чебурашка</option>
-                  <option selected value="Крокодил Гена">Крокодил Гена</option>
-                  <option value="Шапокляк">Шапокляк</option>
-                  <option value="Крыса Лариса">Крыса Лариса</option>
-                 </select></p>
-                 <p><input type="submit" value="Отправить"></p>
-                </form>
+               
                         
                </div>
                <div class = "choosen_tags">
