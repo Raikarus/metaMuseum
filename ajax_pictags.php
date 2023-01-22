1ab470569cea3a978cc4158c7ac670887dee83c9
Raikarus <ivatim32@yandex.ru>
2023-01-22 18:53:24 +0300

    Update ajax_pictags.php

--- ajax_pictags.php
+++ ajax_pictags.php
@@ -80,11 +80,34 @@ function load_cross_kwords()
     $pic_id_from_local_podborka = explode("|", $_POST['img_string']);
     if($pic_id_from_local_podborka)
     {
-        echo "1";
+        if(count($pic_id_from_local_podborka)-1==1)
+        {
+            //ЕСЛИ ВСЕГО ОДНА КАРТИНКА ВЫБРАНА
+            $query = "SELECT tag_id_num FROM pictags WHERE pic_id=$pic_id_from_local_podborka[0] AND tag_id=10";
+            $res = pg_query($cn,$query);
+            $row = pg_fetch_object($res);
+            $tag_id_num = $row->tag_id_num;
+            $query = "SELECT kword_name FROM kwords WHERE tag_id_num=$tag_id_num";
+            $res = pg_query($cn,$query);
+            $row = pg_fetch_object($res);
+            $kword_name = $row->kword_name;
+            //СКОПИРОВАТЬ СТИЛИ ИЛИ ДОБАВИТЬ ДУБЛИКАТ СВОИХ
+            echo "<li>$kword_name</li>";
+        }
+        else
+        {
+            //ЕСЛИ ВЫБРАНО НЕСКОЛЬКО КАРТИНОК
+            $query = "SELECT tag_id_num FROM pictags WHERE pic_id=$pic_id_from_local_podborka[0] AND tag_id=10";
+            for ($i=1; $i < count($pic_id_from_local_podborka)-1; $i++) { 
+                $query .= " UNION SELECT tag_id_num FROM pictags WHERE tag_id=10 AND pic_id=$pic_id_from_local_podborka";               
+            }
+            echo "ЗАПРОСИК $query";
+        }
     }
     else
     {
-        echo "2";
+        //Добавить стили или удалить строчку
+        echo "<b style='color:white'>Ничего не выбрано</b>";
     }
     //$query = "SELECT tag_id_num FROM pictags WHERE (tag_id=10)"
 }
