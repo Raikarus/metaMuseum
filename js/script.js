e5d6e6add353420aedcbe02c7121efcb706bd4cc
Raikarus <ivatim32@yandex.ru>
2023-01-22 05:02:58 +0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -595,7 +595,6 @@ $(document).ready(function(){
     }
 
     data = {'action':'set_podborka_value', 'podborka':podborka_string,'result_tags':result_tags_string,'result_tags_invers':result_tags_invers_string,'size':size,'pre_podborka':pre_podborka_string,'selected_in_podborka':selected_in_podborka_string,'mod':mod};
-   
     $.post(ajaxurl,data).done(function(responce){
       alert(responce);
     });
