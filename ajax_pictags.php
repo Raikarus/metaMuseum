bc0004e0547659c08c1b1aed5b42fa07f5b73f07
Raikarus <ivatim32@yandex.ru>
2023-01-22 19:59:33 +0300

    Update ajax_pictags.php

--- ajax_pictags.php
+++ ajax_pictags.php
@@ -89,10 +89,12 @@ function load_cross_kwords()
             $tag_id_num = $row->tag_id_num;
             $query = "SELECT kword_name FROM kwords WHERE tag_id_num=$tag_id_num";
             $res = pg_query($cn,$query);
-            $row = pg_fetch_object($res);
-            $kword_name = $row->kword_name;
-            //СКОПИРОВАТЬ СТИЛИ ИЛИ ДОБАВИТЬ ДУБЛИКАТ СВОИХ
-            echo "<li>$kword_name</li>";
+            while($row = pg_fetch_object($res))
+            {
+                $kword_name = $row->kword_name;
+                //СКОПИРОВАТЬ СТИЛИ ИЛИ ДОБАВИТЬ ДУБЛИКАТ СВОИХ
+                echo "<li>$kword_name</li>";    
+            }
         }
         else
         {
