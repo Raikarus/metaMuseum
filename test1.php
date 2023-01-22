3e92ea163f8ea33ad592a82d26749ba51186a0a2
mikl14 <mikl.eli2002@gmail.com>
2023-01-23 00:11:35 +0300

    Update test1.php

--- test1.php
+++ test1.php
@@ -57,13 +57,13 @@
           
             var clickBtnValue = $(this).is(":checked");
             
-           
-          console.log($(this).data('str'));
+          
+
             if(clickBtnValue=='0')
             {
              // $(this).data('val',1);
-              
-            
+               console.log($(this).data('str'));
+             console.log(tags_del[0]);
               
             
                for(var i = 0; i < (document.getElementsByName("tags_button")).length;i++)
