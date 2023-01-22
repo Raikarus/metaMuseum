7e3915f9a8cb367f1c6ee0adbf316dd6a264a776
mikl14 <mikl.eli2002@gmail.com>
2023-01-22 23:07:40 +0300

    Update test1.php

--- test1.php
+++ test1.php
@@ -82,7 +82,7 @@
                   {
                     try
                     {
-                       tags[i].innerHTML = ' <label name="tags_button" data-val = "1" data-str = sss><input type="checkbox" name = "transparent_check_box" class="transparent_check_box" checked><span class = "cloud_tag" style="font-size:30 px;">'+$(tags[i]).data('str')+'</span></label>';
+                       tags[i].innerHTML = ' <label name="tags_button" data-val = "1" data-str = sss><input type="checkbox" name = "transparent_check_box"  data-str = '+$(tags[i]).data('str')+'class="transparent_check_box" checked><span class = "cloud_tag" style="font-size:30 px;">'+$(tags[i]).data('str')+'</span></label>';
                     }
                     catch
                     {
@@ -94,7 +94,7 @@
                   {
                     try
                     {
-                       tags[i].innerHTML = ' <label name="tags_button" data-val = "0" data-str = sss><input type="checkbox" name = "transparent_check_box" class="transparent_check_box"><span class = "cloud_tag" style="font-size:30 px;color: #CD5C5C;">'+$(tags[i]).data('str')+'</span></label>';
+                       tags[i].innerHTML = ' <label name="tags_button" data-val = "0"><input type="checkbox" name = "transparent_check_box"  data-str = '+$(tags[i]).data('str')+' class="transparent_check_box"><span class = "cloud_tag" style="font-size:30 px;color: #CD5C5C;">'+$(tags[i]).data('str')+'</span></label>';
                     }
                     catch
                     {
