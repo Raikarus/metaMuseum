676c712416a8273cda31627743c266154475bb64
Raikarus <ivatim32@yandex.ru>
2023-01-23 00:52:20 +0300

    1

--- image_tags.php
+++ image_tags.php
@@ -69,11 +69,12 @@ $(document).ready(function(){
 
     $('#compilation').on("click",".compilation_li .comp_li_button",function(){
       var clickBtnValue = $(this).data('val');
+      console.log(clickBtnValue);
       if(clickBtnValue=='0')
       {
         $(this).css('background-color', '#24B47E');
         $(this).data('val','1');
-
+        $(this).attr('data-val',"1");
         selected_images.push(String($(this).data('img')));
         selected_images_id.push(String($(this).data('id')));
       }
@@ -81,7 +82,7 @@ $(document).ready(function(){
       {
         $(this).css('background-color', 'rgba(255, 255, 255, 0)');
         $(this).data('val','0');
-
+        $(this).attr('data-val',"1");
         selected_images.splice(selected_images.indexOf($(this).data('img')),1);
         selected_images_id.splice(selected_images_id.indexOf($(this).data('id')),1);
       }
