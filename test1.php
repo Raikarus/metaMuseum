20cc1821f40a2231429423c51935e673670808e0
mikl14 <mikl.eli2002@gmail.com>
2023-01-24 17:36:16 +0300

    Update test1.php

--- test1.php
+++ test1.php
@@ -108,7 +108,7 @@
                  
                     try
                     {
-                       tags_normal[i].innerHTML = ' <label name="tags_button" data-val = "1" data-str = sss><input type="checkbox" name = "transparent_check_box" class="transparent_check_box" data-str = '+$(tags_normal[i]).data('str')+' checked><span class = "cloud_tag" style="font-size:30 ;">'+$(tags_normal[i][i]).data('str')+'</span></label>';
+                       tags_normal[i].innerHTML = ' <label name="tags_button" data-val = "1" data-str = sss><input type="checkbox" name = "transparent_check_box" class="transparent_check_box" data-str = '+$(tags_normal[i]).data('str')+' checked><span class = "cloud_tag" style="font-size:30 ;">'+$(tags_normal[i]).data('str')+'</span></label>';
 
                        tags_del[i].innerHTML = ' <label name="tags_button" data-val = "0"><input type="checkbox" name = "transparent_check_box" class="transparent_check_box" data-str = '+$(tags_del[i]).data('str')+' ><span class = "cloud_tag" style="font-size:30 ;color: #CD5C5C;">'+$(tags_del[i]).data('str')+'</span></label>';
                     }
