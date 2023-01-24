1dc73b9c3fe85f69b42c4221b990f269bd7606d8
mikl14 <mikl.eli2002@gmail.com>
2023-01-24 22:31:14 +0300

    Update ajax_tags.php

--- ajax_tags.php
+++ ajax_tags.php
@@ -19,7 +19,7 @@ function load_tags()
      	$status = $row -> status;
      	$responce_str .=$kword_name.'|'.$status.',';
      }
-     $responce_str = substr($responce_str,0,(count($responce_str)-1));
+     $responce_str = substr($responce_str,0,-1);
      echo $responce_str;
 
 }
