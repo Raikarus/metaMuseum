861d410db63fa1ba86fbaec3114bbdd266e4a2dc
mikl14 <mikl.eli2002@gmail.com>
2023-01-26 15:37:33 +0300

    Update tag_editor.php

--- tag_editor.php
+++ tag_editor.php
@@ -227,9 +227,9 @@
                   <div class="normal_tags" id = "norm_tags">
 
                   </div>
-                  <div class="normal_tags" style="border-left: 1px solid white; " id = "del_tags">
+                  <div class="normal_tags" style="border-top: 1px solid white; " id = "del_tags">
                   </div>
-                  <div class="normal_tags" style="border-left: 1px solid white; " id = "del_tags">
+                  <div class="normal_tags" style="border-top: 1px solid white; " id = "del_tags">
                
                     
                   </div>
