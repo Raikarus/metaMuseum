f04f63b61d16931f125831d88c0f2cfc284cdc70
Raikarus <ivatim32@yandex.ru>
2023-01-28 10:41:33 +0300

    Update ajax_pictags.php

--- ajax_pictags.php
+++ ajax_pictags.php
@@ -421,13 +421,12 @@ function add_to_bd($filename,$fsize,$ext) {
     // echo "ЗАПРОСИК $last_query<br>";
 
     $shl = 'mv img_to_download/'.addcslashes($filename," ")." img_to_download/$pic_id.$ext";
-    // echo "Попытка переименовать $shl <br>";
+    echo "Попытка переименовать $shl <br>";
     $res = shell_exec($shl);
-    // echo "$shl <br>$res<br>";
+    echo "$shl <br>$res<br>";
     $shl = 'mv img_to_download/$pic_id.$ext img';
     $res = shell_exec($shl);
-    
-    echo "$pic_id|";
+    //echo "$pic_id|";
   }
   else
   {
