4ecfd97b4737a5361a16ce24faf796a4500fc97c
mikl14 <mikl.eli2002@gmail.com>
2023-02-02 16:01:32 +0300

    Create drone.php

---
+++ drone/drone.php
@@ -0,0 +1,41 @@
+<html>
+ <head>
+  <title>AI</title>
+  <meta charset="utf-8">  
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+<style type="text/css">
+      body
+      {
+        background-image: url(bg.jpg);
+        background-size: 100% auto;
+        margin:0;
+        padding: 0;
+        min-height: 100vh;
+        background-repeat: no-repeat;
+        background-position: center;
+        background-size: cover;
+      }
+      #main
+      {
+        height: 100vh;
+        width: 100%;
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
+      }
+     
+</style>
+  
+    <script>
+
+
+    </script>
+
+ </head>
+
+ <body>
+    <div id = "main">
+
+    </div>
+ </body>
+</html>
